f923b79fece77e4d575d2e84c8664367a5e37c63
davco01a <dacohen@us.ibm.com>
2021-04-28 12:16:35 -0400

    compile java

---
+++ compile.sh
@@ -0,0 +1,3 @@
+wget -nv https://repo1.maven.org/maven2/org/osgi/org.osgi.core/6.0.0/org.osgi.core-6.0.0.jar
+javac -cp .:org.osgi.core-6.0.0.jar java-websphere-traditional/image/project/util/RangeIncludesVersion.java
+
